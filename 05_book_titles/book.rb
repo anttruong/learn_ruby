a86c55579c9a5d0fb3a4258f6f62e45d7796144c
Anthony Truong <gonnahurttony@gmail.com>
2019-08-12 22:26:58 -0700

    done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,6 +1,14 @@
 class Book
-# write your code here
+  attr_reader :title
+  def title=(str)
+    array = str.split()
+    result = array.map{|word| if not (["the", "and", "over", "a", "an", "in", 
+      "of"].include? word) then word.capitalize else word end}
+    result[0] = result[0].capitalize
+    @title = result.join(" ")
+  end
 end
+
 def titleize(str)
   array = str.split()
   result = array.map{|word| if not (["the", "and", "over"].include? word) then \
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize()
+    @seconds=0
+  end
+
+  def time_string()
+    secs = @seconds % 60
+    mins = (@seconds/60%60).floor
+    hrs = (@seconds/3600).floor
+    ("00"+hrs.to_s)[-2..-1]+":"+("00"+mins.to_s)[-2..-1]+":"+("00"+secs.to_s)[-2..-1]
+  end
 end
